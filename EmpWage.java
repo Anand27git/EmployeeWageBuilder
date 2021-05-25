cf8dd54fac074b54dbe715c90e83ee173440ac22
Anand27git <anandkumar256k@gmail.com>
2021-05-25 12:18:27 +0530

    Added Daily Wage

--- EmpWage.java
+++ EmpWage.java
@@ -1,13 +1,29 @@
-public class EmpWage {
+public class EmpWage
+{
+public static void main(String args[])
+{
+System.out.println("Welcome to Employee Wage calculation");
+ 
+int IS_FULL_TIME=1;
+int WORKING_HOUR =20;
 
-public static void main (String[] args)
- {
-  System.out.println("Welcome to Employee Wage Calculation");
-	int IS_Full_TIME = 1;
-double empCheck = Math.floor(Math.random() * 10 ) % 2;
-if (empCheck ==IS_Full_TIME)
-System.out.println("Employee is Present");
-else
-System.out.println("Employee is Absent");
-  }
+int empHrs=0;
+int empWage=0;
+
+double empCheck = Math.floor(Math.random() * 10) % 2;
+
+if  (empCheck == IS_FULL_TIME)
+{
+	empHrs=8;
+	System.out.println("Employee is present");
 }
+else
+{
+
+	System.out.println("Employee is Absent");
+}
+empWage=empHrs*WORKING_HOUR;
+System.out.println("Employee DailyWage:" + empWage);
+}
+}
+
