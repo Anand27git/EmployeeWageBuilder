474c11b9ccc37eb09fce5d66f738fcdf506e9a1a
Anand27git <anandkumar256k@gmail.com>
2021-05-25 12:06:53 +0530

    Employee Attendence

--- EmpWage.java
+++ EmpWage.java
@@ -1,7 +1,13 @@
 public class EmpWage {
-public static void main(String args[])
+
+public static void main (String[] args)
  {
   System.out.println("Welcome to Employee Wage Calculation");
-
+	int IS_Full_TIME = 1;
+double empCheck = Math.floor(Math.random() * 10 ) % 2;
+if (empCheck ==IS_Full_TIME)
+System.out.println("Employee is Present");
+else
+System.out.println("Employee is Absent");
   }
 }
