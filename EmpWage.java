c60507bd15bdc64a76f1b0e0cb4658662e15aefb
Anand27git <anandkumar256k@gmail.com>
2021-05-25 12:59:50 +0530

    calculate total Wage per month

--- EmpWage.java
+++ EmpWage.java
@@ -3,6 +3,7 @@ public class EmpWage {
 public static final int IS_FULL_TIME = 1;
 public static final int IS_PART_TIME = 2;
 public static final int EMP_RATE_PER_HOUR = 20;
+public static final int Num_Of_Working_Days = 2;
 
 public static void main(String args[])
 {
@@ -11,7 +12,9 @@ System.out.println("Welcome to Employee Wage calculation");
 // variables
 int empHrs=0;
 int empWage=0;
+int totalEmpWage=0;
 //Computation
+for (int day=0; day < Num_Of_Working_Days; day++) {
 int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 switch(empCheck) {
 
@@ -29,7 +32,10 @@ switch(empCheck) {
 	System.out.println("Employee is Absent");
 }
 empWage = empHrs*EMP_RATE_PER_HOUR;
+totalEmpWage += empWage;
 System.out.println("Employee Wage:"+ empWage);
+}
+System.out.println("Total Employee Wage:" + totalEmpWage);
   }
  
 }
