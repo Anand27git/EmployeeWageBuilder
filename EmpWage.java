caa9fb8f12593abd206527b510824be87b0ce2ad
Anand27git <anandkumar256k@gmail.com>
2021-05-25 12:45:34 +0530

    EmpWage Using Switch Case

--- EmpWage.java
+++ EmpWage.java
@@ -1,33 +1,35 @@
 public class EmpWage {
 
+public static final int IS_FULL_TIME = 1;
+public static final int IS_PART_TIME = 2;
+public static final int EMP_RATE_PER_HOUR = 20;
+
 public static void main(String args[])
 {
 System.out.println("Welcome to Employee Wage calculation");
  
-int IS_FULL_TIME=1;
-int IS_PART_TIME=2;
-int WORKING_HOUR =20;
-
+// variables
 int empHrs=0;
 int empWage=0;
-double empCheck = Math.floor(Math.random() * 10) % 3;
+//Computation
+int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+switch(empCheck) {
 
-if (empCheck == IS_FULL_TIME)
-{
+ case IS_FULL_TIME:
 	empHrs=8;
 	System.out.println("Employee is present as Full time");
-}
-else if (empCheck == IS_PART_TIME)
-{
-	empHrs=8;
+	break;
+ case IS_PART_TIME:
+
+	empHrs=4;
 	System.out.println("Employee is working as part time");
-}
-else
-{
+	break;
+ default:
 	empHrs=0;
 	System.out.println("Employee is Absent");
 }
-empWage=empHrs*WORKING_HOUR;
+empWage = empHrs*EMP_RATE_PER_HOUR;
 System.out.println("Employee Wage:"+ empWage);
-}
+  }
+ 
 }
