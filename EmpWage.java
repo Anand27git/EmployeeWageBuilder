49cae3802fe1f6090ec016c8280e9a999a4d86ea
Anand27git <anandkumar256k@gmail.com>
2021-05-31 15:58:15 +0530

    Employee wage using oops Concept

--- EmpWage.java
+++ EmpWage.java
@@ -1,46 +1,53 @@
 public class EmpWage {
+	// declaring static variables
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
 
-public static final int IS_FULL_TIME = 1;
-public static final int IS_PART_TIME = 2;
-public static final int EMP_RATE_PER_HOUR = 20;
-public static final int Num_Of_Working_Days = 5;
-public static final int MAX_HRS_IN_MONTH = 10 ;
+	public static int EmployeeWageforCompany(String company, int wagePerHr, int maxWorkingHr, int maxWorkingDays) {
+		// declaring the variables
+		int totalWage = 0;
+		int totalWorkingDays = 0;
+		int totalHrs = 0;
+		// checking with while loop 
+		while (totalWorkingDays < maxWorkingDays && totalHrs < maxWorkingHr) {
 
-public static void main(String args[])
-{
-System.out.println("Welcome to Employee Wage calculation");
- 
-// variables
-int empHrs=0;
-int totalEmpHrs=0;
-int totalWorkingDays=0;
-//Computation
-    while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
-          totalWorkingDays < Num_Of_Working_Days) {
-               
-                totalWorkingDays++;
-int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-     
-         switch(empCheck) {
+			int empHrs = 0;
+			totalWorkingDays++;
 
-          case IS_FULL_TIME:
-         	empHrs=8;
-	      System.out.println("Employee is present as Full time");
-	         break;
-          case IS_PART_TIME:
+			int empCheck = (int) (Math.random() * 3);
+			// using switch statement to check the employee working time
+			switch (empCheck) {
 
-	        empHrs=4;
-	       System.out.println("Employee is working as part time");
-	         break;
-    	default:
-	empHrs=0;
-	System.out.println("Employee is Absent");
-       }
-	totalEmpHrs +=empHrs;
-	System.out.println("Day#: " + totalWorkingDays + " Emp Hr:" + empHrs);
-     }
-int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-System.out.println("Total Employee Wage:" + totalEmpWage);
-  }
- 
+			case IS_FULL_TIME:
+				empHrs = 8;
+				System.out.println("Employee is present as Full time");
+				break;
+			case IS_PART_TIME:
+
+				empHrs = 4;
+				System.out.println("Employee is working as part time");
+				break;
+			default:
+				empHrs = 0;
+				System.out.println("Employee is Absent");
+			}
+			// calculating the daily wage of an employee
+			totalHrs += empHrs;
+			int dailyWage = empHrs * wagePerHr;
+			System.out.println("Daily Wage =" + dailyWage);
+		}
+		// calculating total Employee wage for company
+		totalWage = totalHrs * wagePerHr;
+		System.out.println("Total Employee Wage for Company:" + company + " is " + totalWage);
+		return totalWage;
+	}
+
+	public static void main(String args[]) {
+
+		System.out.println("Welcome to Employee Wage calculation");
+
+		EmployeeWageforCompany(" Demart ", 5, 25, 25);
+		EmployeeWageforCompany(" Reliance ", 5, 25, 20);
+	}
+
 }
